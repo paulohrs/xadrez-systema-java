811f8b2163fec67ff949506c0ddcd1c11f195bcd
Paulo Henrique <paulohrs@hotmail.com>
2021-10-01 08:15:49 -0300

    tratamento de erros

--- src/tabuleirojogo/Tabuleiro.java
+++ src/tabuleirojogo/Tabuleiro.java
@@ -1,5 +1,7 @@
 package tabuleirojogo;
 
+import xadrez.BoardException;
+
 public class Tabuleiro {
 
 	private int linhas;
@@ -7,6 +9,9 @@ public class Tabuleiro {
 	private Peca[][] pecas;
 
 	public Tabuleiro(int linhas, int colunas) {
+		if (linhas < 1 || colunas < 1) {
+			throw new BoardException("É necessário que haja pelo menos uma coluna!");
+		}
 		this.linhas = linhas;
 		this.colunas = colunas;
 		pecas = new Peca[linhas][colunas];
@@ -16,29 +21,42 @@ public class Tabuleiro {
 		return linhas;
 	}
 
-	public void setLinhas(int linhas) {
-		this.linhas = linhas;
-	}
-
 	public int getColunas() {
 		return colunas;
 	}
 
-	public void setColunas(int colunas) {
-		this.colunas = colunas;
-	}
-
 	public Peca peca(int linha, int coluna) {
+		if (!posicaoExistente(linha, coluna)) {
+			throw new BoardException("Esta posição não existe no tabuleiro");
+		}
 		return pecas[linha][coluna];
 	}
 
 	public Peca peca(Posicao posicao) {
+		if (!posicaoExistente(posicao)) {
+			throw new BoardException("Esta posição não existe no tabuleiro");
+		}
 		return pecas[posicao.getLinha()][posicao.getColuna()];
 	}
-	
+
 	public void lugarPeca(Peca peca, Posicao posicao) {
+		if (temUmaPeca(posicao)) {
+			throw new BoardException("Já existe uma peça nesta posição " + posicao);
+		}
 		pecas[posicao.getLinha()][posicao.getColuna()] = peca;
 		peca.posicao = posicao;
+	}
+
+	private boolean posicaoExistente(int linha, int coluna) {
+		return linha >= 0 && linha < linhas && coluna >= 0 && coluna < colunas;
+	}
+
+	public boolean posicaoExistente(Posicao posicao) {
+		return posicaoExistente(posicao.getLinha(), posicao.getColuna());
+	}
+
+	public boolean temUmaPeca(Posicao posicao) {
+		return peca(posicao) != null;
 	}
 
 }
