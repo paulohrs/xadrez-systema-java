48237a6a99a49de6b33819b86336a98ef4730c4e
Paulo Henrique <paulohrs@hotmail.com>
2021-10-05 11:43:07 -0300

    Limpa tela e move peças

--- src/Aplicacao/Jogo.java
+++ src/Aplicacao/Jogo.java
@@ -1,7 +1,9 @@
 package Aplicacao;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import xadrez.XadrezException;
 import xadrez.XadrezPartida;
 import xadrez.XadrezPeca;
 import xadrez.XadrezPosicao;
@@ -15,17 +17,27 @@ public class Jogo {
 		XadrezPartida xadrezPartida = new XadrezPartida();
 
 		while (true) {
-			UI.printTabuleiro(xadrezPartida.getPecas());
-			System.out.println();
-			System.out.print("Posição de Origem: ");
-			XadrezPosicao procura = UI.lerXadrezPosicao(sc);
-			
-			System.out.println();
-			System.out.print("Posição Destino: ");
-			XadrezPosicao alvo = UI.lerXadrezPosicao(sc);
+			try {
+				UI.clearScreen();
+				UI.printTabuleiro(xadrezPartida.getPecas());
+				System.out.println();
+				System.out.print("Posição de Origem: ");
+				XadrezPosicao procura = UI.lerXadrezPosicao(sc);
+				
+				System.out.println();
+				System.out.print("Posição Destino: ");
+				XadrezPosicao alvo = UI.lerXadrezPosicao(sc);
 			
 			XadrezPeca capturaPeca = xadrezPartida.performeXadrezMover(procura, alvo);
-			sc.close();
+			}
+			catch (XadrezException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
+			catch (InputMismatchException e) {
+				System.out.println(e.getMessage());
+				sc.nextLine();
+			}
 		}
 	}
 
